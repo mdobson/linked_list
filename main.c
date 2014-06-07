cce8696965460003aac78376cdfa014fae0833d9
Matt Dobson <mdobson4@gmail.com>
2014-06-06 21:17:17 -0400

    Did a small refactor. Added in a list struct. Added in an iterator function. Repointed function pointers.

--- main.c
+++ main.c
@@ -4,34 +4,49 @@
 struct node {
   int x;
   struct node *next;
-  void (*add)(struct node* p_root, struct node*p_node);
-  void (*delete)(struct node* p_root, int value);
 };
 
-struct node * new();
-void add(struct node *p_root, struct node *p_node);
-void delete(struct node *p_root, int value);
+struct list {
+  struct node *head;
+  int count;
+  void (*add)(struct list *p_list, struct node *p_node);
+  void (*delete)(struct list *p_list, int value);
+  void (*iterate)(struct list *p_list, void (*f)(int));
+};
 
-struct node * new() {
+struct list * newList();
+struct node * newNode();
+void add(struct list *p_list, struct node *p_node);
+void delete(struct list *p_list, int value);
+void iterate(struct list *p_list, void (*f)(int));
+
+struct node * newNode() {
   struct node * newNode = (struct node *) malloc(sizeof(struct node));
-  newNode -> add = add;
-  newNode -> delete = delete;
+  newNode -> next = 0;
   return newNode;
 }
 
-void add(struct node * p_root, struct node *p_node) {
+struct list * newList() {
+  struct list *newList = (struct list *) malloc(sizeof(struct list));
+  newList -> add = add;
+  newList -> delete = delete;
+  newList -> iterate = iterate;
+  return newList;
+}
+
+void add(struct list *p_list, struct node *p_node) {
   struct node *cur;
-  cur = p_root;
+  cur = p_list -> head;
   while(cur -> next != 0) {
     cur = cur -> next;
   }
   cur -> next = p_node;
 }
 
-void delete(struct node * p_root, int val) {
+void delete(struct list * p_list, int val) {
   struct node *cur;
   struct node * prev;
-  cur = p_root;
+  cur = p_list -> head;
   while(cur -> x != val) {
     prev = cur;
     cur = cur -> next;
@@ -41,27 +56,33 @@ void delete(struct node * p_root, int val) {
   free(cur);
 }
 
-int main() {
-  struct node *root;
-  struct node *conductor;
+void iterate(struct list *p_list, void (*f)(int)) {
   struct node *iter;
-
-  root = new();
-  root -> next = 0;
-  root -> x = 5;
-  conductor = new();
-  conductor -> next = 0;
-  conductor -> x = 6;
-  root -> add(root, conductor);
-  conductor = new();
-  conductor -> next = 0;
-  conductor -> x = 7;
-  root -> add(root, conductor);
-  root -> delete(root, 6);
-
-  iter = root;
+  iter = p_list -> head;
   while(iter != 0) {
-    printf("Value:%d\n", iter -> x);
+    (*f)(iter -> x);
     iter = iter -> next;
   }
+}
+
+void print(int x) {
+  printf("PRINTING: %d\n", x);
+}
+
+int main() {
+  struct list *list;
+  struct node *root;
+  struct node *second;
+
+  list = newList();
+  root = newNode();
+  second = newNode();
+  root -> x = 1;
+  list -> head = root;
+  
+  second -> x = 2;
+
+  list -> add(list, second);
+  list -> iterate(list, print);
+  
 }
